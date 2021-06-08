e5230f95c86bfb135ff77fc4b78b7340cac67941
vikzelikov <vikzelikov@yandex.ru>
2021-06-08 18:36:10 +0300

    feat: base arch

---
+++ Genies/Presentation/Modules/Authorization/View/AuthViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Genies
+//
+//  Created by Yegor on 07.06.2021.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func nextButtonDidPress(_ sender: Any) {
+//        let homeStoryboard = UIStoryboard(name: "Home", bundle: nil)
+//        guard let homePage = homeStoryboard.instantiateViewController(withIdentifier: "HomeViewController") as? HomeViewController else { return }
+//        
+//        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+//        appDelegate.window?.rootViewController = homePage
+    }
+    
+}
+
