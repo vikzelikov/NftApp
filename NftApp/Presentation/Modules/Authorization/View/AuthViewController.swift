0848d380ba01ffe4c462400bd64b38055c048467
vikzelikov <vikzelikov@yandex.ru>
2021-08-25 00:07:20 +0300

    bug fixes

--- NftApp/Presentation/Modules/Authorization/View/AuthViewController.swift
+++ NftApp/Presentation/Modules/Authorization/View/AuthViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import PassKit
 
 class AuthViewController: UIViewController {
 
@@ -34,12 +35,59 @@ class AuthViewController: UIViewController {
     }
     
     @IBAction func signupDidTap(_ sender: Any) {
+//        startApplePay()
         let homeStoryboard = UIStoryboard(name: "Authorization", bundle: nil)
         guard let vc = homeStoryboard.instantiateViewController(withIdentifier: "SignUpViewController") as? SignUpViewController else { return }
         navigationController?.pushViewController(vc, animated: true)
     }
     
+   
+ 
+    func displayDefaultAlert(title: String?, message: String?) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+       let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func startApplePay() {
+        // Open Apple Pay purchase
+        let shoe = Shoe(name: "Карточка #213", price: 0.11)
+        let paymentItem = PKPaymentSummaryItem.init(label: shoe.name, amount: NSDecimalNumber(value: shoe.price))
+        let paymentNetworks = [PKPaymentNetwork.amex, .discover, .masterCard, .visa]
+        
+        if PKPaymentAuthorizationViewController.canMakePayments(usingNetworks: paymentNetworks) {
+            let request = PKPaymentRequest()
+            request.currencyCode = "USD" // 1
+            request.countryCode = "US" // 2
+            request.merchantIdentifier = "merchant.com.nft.dev" // 3
+            request.merchantCapabilities = PKMerchantCapability.capability3DS // 4
+            request.supportedNetworks = paymentNetworks // 5
+            request.paymentSummaryItems = [paymentItem] // 6
+            
+            guard let paymentVC = PKPaymentAuthorizationViewController(paymentRequest: request) else {
+                displayDefaultAlert(title: "Error", message: "Unable to present Apple Pay authorization.")
+                return
+            }
+            
+            paymentVC.delegate = self
+            self.present(paymentVC, animated: true, completion: nil)
+        }
+    }
+    
 }
 
+struct Shoe {
+    var name: String
+    var price: Double
+}
 
-
+extension AuthViewController: PKPaymentAuthorizationViewControllerDelegate {
+    func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
+        dismiss(animated: true, completion: nil)
+        displayDefaultAlert(title: "Success!", message: "The Apple Pay transaction was complete.")
+    }
+}
